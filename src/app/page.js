fafaaf462a4eeeccb6f490bfacfedf901496a9e5
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-01-31 23:35:25 +0600

    set up

--- src/app/page.js
+++ src/app/page.js
@@ -45,7 +45,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Read our docssss
           </a>
         </div>
       </main>
