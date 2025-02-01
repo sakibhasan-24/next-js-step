fea4a2d89d7b741bf98e1a3dc0c57c285b74e0c3
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-02-01 23:32:53 +0600

    dynamic routes implements

---
+++ src/app/Login/page.js
@@ -0,0 +1,5 @@
+export default function Login() {
+  return <div className="text-4xl font-semibold my-8 p-6">Login</div>;
+}
+
+
---
+++ src/app/products/[product]/page.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SingleProduct() {
+  return (
+    <div>
+      <h1 className="text-4xl text-center">this is Single Products</h1>
+    </div>
+  );
+}
